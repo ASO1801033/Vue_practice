0f25d67d8ed6ea70b6f2450269e029b985997040
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-03-07 23:42:35 +0900

    7th-commit

---
+++ index_2.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#menu',
+  data: {
+    osushi: [
+      {name: 'イカ', qty: 2, price: 100},
+      {name: '大トロ', qty: 1, price: 1000},
+      {name: 'イクラ', qty: 2, price: 100},
+      {name: 'あなご', qty: 1, price: 200},
+      {name: 'アジ', qty: 2, price: 100},
+      {name: 'ヒラメ', qty: 1, price: 200}
+    ]
+  }
+});
