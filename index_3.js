5c798f7837440ed17ea748df97e6b2a35b8c9771
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-03-11 23:23:20 +0900

    13th-commit

--- index_3.js
+++ index_3.js
@@ -12,8 +12,10 @@ const vue = new Vue({
       email:'',
     },
     users: [],
+    editIndex: -1, //update_userで使う
+    createFlag: true, //モーダルのボタン切り替え
   },
-  created() {
+  created(){
     this.users = [
       {
         id: 1,
@@ -23,18 +25,35 @@ const vue = new Vue({
     ]
   },
   methods: {
-    showModal() { //モーダルを表示
-        this.$modal.show('user-modal');
+    showModal(){ //モーダルを表示
+      this.createFlag = true;
+      this.resetForm();
+      this.$modal.show('user-modal');
     },
-    add_user() { //メンバー登録
+    add_user(){ //メンバー登録
       const user = Object.assign({},this.form); //入力した値からuserオブジェクトを作成
       this.users.push(user); //配列のpushメソッドを使って配列usersの一番後ろに作成したuserオブジェクトを追加
       this.$modal.hide('user-modal'); //追加が完了するとthis.$modal.hideでモーダルウィンドウを非表示
+      this.resetForm(); //resetFormの呼び出し
+    },
+    delete_user(user){ //メンバー削除
+      const index = this.users.indexOf(user); //indexOfメソッドを使って削除を行ったメンバーの配列の番号を取得
+      this.users.splice(index, 1) //spliceメソッドを使ってindex番目の要素1つを配列から削除
+    },
+    edit_user(user){ //メンバー更新
+      this.createFlag = false,
+      this.editIndex = this.users.indexOf(user);
+      this.form = Object.assign({}, user);
+      this.$modal.show('user-modal');
+    },
+    update_user(){ //モーダルウィンドウ上で行ったユーザ情報の更新を保存
+      Object.assign(this.users[this.editIndex], this.form);
+      this.$modal.hide('user-modal');
     },
     resetForm(){ //モーダル内の入力値をリセット
-        this.form.id = '';
-        this.form.name = '';
-        this.form.email = '';
+      this.form.id = '';
+      this.form.name = '';
+      this.form.email = '';
     },
   },
-})
+});
--- readme.md
+++ readme.md
@@ -4,7 +4,13 @@
  index_1.html / index_1.js<br>
  => ソースの写経
 </li>
+
 <li>
  index_2.html / index_2.js / index_2.css<br>
  => 配列要素の表示 & フィルタリングとソート機能実装
+</li>
+
+<li>
+ index_3.html / index_3.js<br>
+ => SPAの練習(メンバーの登録・削除・更新)
 </li>
