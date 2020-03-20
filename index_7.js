809f28384c3ac3c95b1450e03cdf10784082cafe
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-03-20 23:35:41 +0900

    22th-commit

--- index_7.js
+++ index_7.js
@@ -6,3 +6,23 @@ new Vue({
     ok2: true,
   }
 });
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    fruits: [
+      { name: 'cherry' },
+      { name: 'grape' },
+      { name: 'banana' },
+      { name: 'melon' },
+      { name: 'peach' }
+    ]
+  }
+});
+app2.fruits.push({ name: 'apple ← It\'s push-New!' }); //末尾に追加
+app2.fruits.pop(); //末尾を取り出す
+app2.fruits.unshift({ name: 'strawberry ← It\'s unshift-New!' }); //先頭に追加
+app2.fruits.shift(); //先頭を取り出す
+app2.fruits.splice(2, 0, { name: 'ピーマン' }); //splice(何番目に , 挿入=0 or 置き換え=1 , 値)好きな場所に値を挿入
+app2.fruits.splice(2, 1, { name: 'パプリカ' }); //splice(何番目に , 挿入=0 or 置き換え=1 , 値)好きな場所の値を置き換える
+app2.fruits.splice(1, 2); //1番目から2個の値を削除する
