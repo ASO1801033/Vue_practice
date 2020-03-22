4cc2c41113b2ae939fb20d29e286033ee8b40451
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-03-22 12:08:11 +0900

    25th-commit

---
+++ index_9.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    items: [
+      { id: 'all', value: 0, text: 'すべて' },
+      { id: 'doing', value: 1, text: '未完了' },
+      { id: 'done', value: 2, text: '完了' },
+    ],
+    tasks: [],
+    value: '',
+    show: 0,
+  },
+  methods: {
+    add() {
+      this.tasks.push({ //タスクの追加
+        task: this.value,
+        done: false,
+      });
+      this.value = '';
+    },
+    del(index) { //タスクの削除
+      if(confirm('削除しますか？')) {
+        this.task.splice(index, 1); //index番目から1要素を削除する
+      }
+    },
+  },
+  computed: { //条件によって表示したい配列をフィルタリングする時は算出プロパティをつかう
+    showTasks() {
+      switch (this.show) {
+        case 0: //すべて
+          return this.tasks;
+        case 1: //未完了
+          //配列で条件に一致する要素を抜き出すにはfilterメソッドをつかう
+          return this.tasks.filter(e => !e.done); //trueの要素が抜き出される(idがdoneでないのもの)
+        case 2: //完了
+          //配列で条件に一致する要素を抜き出すにはfilterメソッドをつかう
+          return this.tasks.filter(e => e.done); //trueの要素が抜き出される(idがdoneのもの)
+        default:
+          return [];
+      }
+    },
+  },
+});
