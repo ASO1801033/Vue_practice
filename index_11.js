069beed3f7cccbbd8e9644c32cd4f4c669a1fe56
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-05-09 12:00:00 +0900

    61th-commit

--- index_11.js
+++ index_11.js
@@ -1,6 +1,21 @@
+//vue-js-modalを使うためにVue.useを設定
+Vue.use(window["vue-js-modal"].default);
+
 var app  = new Vue({
   el: "#app",
   data: {
     title: '家計簿つけつけマン',
-  }
+    form: {
+      date: '',
+    },
+  },
+  methods: {
+    Modal_show(){ //モーダルを表示
+      this.resetForm();
+      this.$modal.show('pay-modal');
+    },
+    resetForm() { //モーダル内の入力値をリセット
+      this.form.date = '';
+    },
+  },
 });
